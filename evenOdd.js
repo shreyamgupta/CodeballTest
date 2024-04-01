66bd8fde9761458b0a2536fcb2d40516155e84e8
Shreyam Gupta <57482541+shreyamgupta@users.noreply.github.com>
2024-04-01 11:18:56 +0530

    Create evenOdd.js

---
+++ evenOdd.js
@@ -0,0 +1,11 @@
+function checkEvenOrOdd(number) {
+    if(number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+// Example usage:
+console.log(checkEvenOrOdd(10)); // Output: Even
+console.log(checkEvenOrOdd(7));  // Output: Odd
